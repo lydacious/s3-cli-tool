ba7ed80998e77a27d0d1f164132e327f0bbe1e43
Lydacious <raminmasumov04@gmail.com>
2024-07-27 10:02:42 +0400

    Added filter with regex command

--- index.js
+++ index.js
@@ -24,6 +24,28 @@ async function listFiles(bucket, prefix = '') {
     }
 }
 
+// Lists files with regex filter
+async function listFilesWithFilter(bucket, prefix = '', filterRegex = '') {
+    try {
+        const params = {
+            Bucket: bucket,
+            Prefix: prefix
+        };
+        const data = await s3.listObjectsV2(params).promise();
+        if (data.Contents.length === 0) {
+            console.log('No files found');
+        } else {
+            const regex = new RegExp(filterRegex);
+            data.Contents
+                .map(item => item.Key)
+                .filter(key => regex.test(key))
+                .forEach(key => console.log(key));
+        }
+    } catch (err) {
+        console.error('Error listing files with filter:', err);
+    }
+}
+
 // Uploads file
 async function uploadFile(bucket, filePath, key){
     const fileContent = fs.readFileSync(filePath);
@@ -52,6 +74,15 @@ if (process.argv[2] === 'list') {
         process.exit(1);
     }
     listFiles(bucket, prefix);
+} else if (process.argv[2] === 'list-filter') {
+    const bucket = process.argv[3];
+    const prefix = process.argv[4] || '';
+    const filter = process.argv[5] || '';
+    if (!bucket || !filter) {
+        console.error('Bucket name and filter are required');
+        process.exit(1);
+    }
+    listFilesWithFilter(bucket, prefix, filter);
 } else if (process.argv[2] === 'upload') {
     const bucket = process.argv[3];
     const filePath = process.argv[4];
@@ -64,5 +95,6 @@ if (process.argv[2] === 'list') {
 } else {
     console.log('Usage:');
     console.log('  node index.js list <bucket-name> [prefix]');
+    console.log('  node index.js list-filter <bucket-name> [prefix] <filter>');
     console.log('  node index.js upload <bucket-name> <file-path> <key>');
 }
